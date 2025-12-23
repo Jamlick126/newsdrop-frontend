f7baede523b3ca8c3ad3575ef3f9796be095c055
[Jamlick Kariuki] <[karisfxturk@gmail.com]>
2025-12-23 20:30:22 +0300

    attempt to fix category section images

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 
 const CategoryCard = ( { category }) => {
     let categoryImageUrl = category.imageUrl;
-
+    //Remove /public if it exists
+    if (categoryImageUrl && categoryImageUrl.startsWidth('/public')) {
+        categoryImageUrl = categoryImageUrl.replace('/public', '');
+    }
+    // Ensure it starts with a single /
     if (categoryImageUrl && !categoryImageUrl.startsWith('/')) {
         categoryImageUrl = '/' + categoryImageUrl;
     }
@@ -14,14 +18,12 @@ const CategoryCard = ( { category }) => {
     return (
         <Link to={`/category/${category.slug}`} className="article category-card-link" >
             <div className="category-image-box">
-                <img src={categoryImageUrl} alt={category.name} 
+                <img src={`${import.meta.env.VITE_API_URL}${categoryImageUrl}`} alt={category.name} 
                 className="article-image"/>
             </div>
             <div className="category-data">
                 <span className="tag-name">#{category.name}</span>
-            </div>
-          
-            
+            </div>    
         </Link>
     );
 };
